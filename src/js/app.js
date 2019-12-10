045a98e02f75b12ed3fdda2a1f5d20f1bd60059f
SebastianGarces <gsgarces1@gmail.com>
2019-12-09 22:50:15 -0500

    added cards variables to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
 const [navLinkLang, navLinkFrame, navLinkTools] = document.querySelectorAll(
-    ".sidebar a"
+	".sidebar a"
 );
+
+const allCards = document.querySelectorAll(".card");
+
+const reactCard = document.querySelector(".react");
+const vueCard = document.querySelector(".vue");
+const gitCard = document.querySelector(".git");
+const gitlabCard = document.querySelector(".gitlab");
+const jsCard = document.querySelector(".js");
+const nodeCard = document.querySelector(".node");
+const vscodeCard = document.querySelector(".vscode");
+const pythonCard = document.querySelector(".python");
+const htmlCard = document.querySelector(".html");
+const cssCard = document.querySelector(".css");
+const netlifyCard = document.querySelector(".netlify");
